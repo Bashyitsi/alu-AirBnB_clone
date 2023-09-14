ee8fb0cc324d74cb191c3ec45e6824bc87183e84
Your Name <you@example.com>
2023-09-14 07:14:12 -0700

    Test

--- tests/test_models/test_amedity.py
+++ tests/test_models/test_amedity.py
@@ -20,4 +20,7 @@ class TestAmenity(unittest.TestCase):
 
     def test_class_attr(self):
         self.assertIs(type(self.amenity.name), str)
-        self.assertFalse(bool(getattr(self.amenity, "name")))
+        self.assertEqual(self.amenity.name, "")
+
+if __name__ == "__main__":
+    unittest.main()
